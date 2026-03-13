1dc96aa0c35f384d00a8af4d3a04e691c87bbe0b
박성준 <freestarjun@gmail.com>
2026-03-13 13:05:21 +0900

    2026/3/13(2일차과제)

---
+++ src/assets/oneroom.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    id: 0,
+    title: "해운대 해수욕장 도보 5분 원룸",
+    image: "https://images.pexels.com/photos/460672/pexels-photo-460672.jpeg",
+    content: "해운대역 도보 3분, 해수욕장 도보 5분 거리의 풀옵션 원룸입니다.",
+    price: 540000,
+  },
+  {
+    id: 1,
+    title: "광안리 바다 전망 오션뷰 원룸",
+    image: "https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg",
+    content: "광안대교가 보이는 남향 오션뷰 원룸, 야경이 아름다운 집입니다.",
+    price: 650000,
+  },
+  {
+    id: 2,
+    title: "서면롯데백화점 인근 오피스텔",
+    image: "https://images.pexels.com/photos/1571460/pexels-photo-1571460.jpeg",
+    content: "서면 번화가 도보 2분, 직장인에게 인기 많은 오피스텔형 원룸입니다.",
+    price: 720000,
+  },
+  {
+    id: 3,
+    title: "부산대역 출구 앞 캠퍼스 원룸",
+    image: "https://images.pexels.com/photos/271639/pexels-photo-271639.jpeg",
+    content: "부산대학교 정문까지 도보 5분, 학생 전용 풀옵션 원룸입니다.",
+    price: 430000,
+  },
+  {
+    id: 4,
+    title: "남포동/자갈치시장 도보권 원룸",
+    image: "https://images.pexels.com/photos/1643383/pexels-photo-1643383.jpeg",
+    content: "남포역·자갈치시장 근처, 관광지와 가까운 깔끔한 원룸입니다.",
+    price: 560000,
+  },
+  {
+    id: 5,
+    title: "에코델타시티 신축 원룸",
+    image: "https://images.pexels.com/photos/3637729/pexels-photo-3637729.jpeg",
+    content: "강서구 에코델타시티 신축 단지 내 원룸, 조용한 주거 환경입니다.",
+    price: 510000,
+  },
+];
