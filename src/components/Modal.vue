ef02a40d143a4f295268690f33494441a8fb38a2
박성준 <freestarjun@gmail.com>
2026-03-26 23:53:32 +0900

    2026/3/26(3주차과제)

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,12 +1,12 @@
 // Modal.vue
-
 <template>
   <div class="black-bg" v-if="모달창">
     <div class="white-bg">
       <img :src="원룸들[상품번호].image" class="room-image" />
       <h4>{{ 원룸들[상품번호].title }}</h4>
       <p>{{ 원룸들[상품번호].content }}</p>
-      <p>{{ 원룸들[상품번호].price }}원</p>
+      <input v-model="month">
+      <p> {{ month }} 개월 선택함 : {{ 원룸들[상품번호].price * month }}원</p>
       <button @click="$emit('closeModal')">닫기</button>
     </div>
   </div>
@@ -14,12 +14,31 @@
 
 <script>
 export default {
-  name: 'Modal',    
+  name: 'Modal',
+  data(){
+    return {
+      month : '0',
+    }
+  },  
   props:{
     원룸들 : Array,
     상품번호 : Number,
     모달창 : Boolean
-  }
+  },
+  watch : {
+    month(a){
+       if (a === '' || a === null) return;
+
+      if (isNaN(a)){
+        alert('문자입력하지마세요');
+        this.month = 1;
+      }
+      if(a<1 || a>12){
+        alert('1개월 이상 12개월 이하로 입력해주세요');
+        this.month = 1;
+      }
+    },
+ },
   
 }
 </script>
