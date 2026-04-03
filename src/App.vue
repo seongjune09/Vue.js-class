e5a2a3783bb49e1067b46b9d0bf390c5a8323d4a
박성준 <freestarjun@gmail.com>
2026-04-03 22:07:44 +0900

    2026/4/3/Instagram 개발

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+// App.vue
+<template>
+  <div class="header">
+    <ul class="header-button-left">
+      <li>Cancel</li>
+    </ul>
+    <ul class="header-button-right">
+      <li>Next</li>
+    </ul>
+    <img src="./assets/logo.svg" class="logo" />
+  </div>
+
+  <Container />
+
+  <div class="footer">
+    <ul class="footer-button-plus">
+      <input type="file" id="file" class="inputfile" />
+      <label for="file" class="input-plus">+</label>
+    </ul>
+ </div>
+</template>
+
+
+<script>
+import './Post.js';
+
+export default {
+  name : 'App',
+  components : {
+  }
+}
+</script>
---
+++ src/components/Container.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <Post />
+        <Post />
+        <Post />
+    </div>
+</template>
+
+<script>
+import './App.vue'
+
+export default {
+    name: 'App',
+    components: {
+    }
+}
+</script>
