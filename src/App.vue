7b415c18ad33606d6454ddc518cf8f4e78d381f7
박성준 <freestarjun@gmail.com>
2026-04-06 23:52:36 +0900

    2026/4/6/App.vue 수정

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,49 @@
-// App.vue
 <template>
-  <div class="header">
-    <ul class="header-button-left">
-      <li>Cancel</li>
-    </ul>
-    <ul class="header-button-right">
-      <li>Next</li>
-    </ul>
-    <img src="./assets/logo.svg" class="logo" />
+  <div>
+    <div class="header">
+      <ul class="header-button-left">
+        <li>Cancel</li>
+      </ul>
+      <ul class="header-button-right">
+        <li>Next</li>
+      </ul>
+      <img src="./assets/logo.svg" class="logo" />
+    </div>
+
+    <Container :posts="posts" />
+    <button @click="more">더보기</button>
+
+    <div class="footer">
+      <ul class="footer-button-plus">
+        <input type="file" id="file" class="inputfile" />
+        <label for="file" class="input-plus">+</label>
+      </ul>
+    </div>
   </div>
-
-  <Container />
-
-  <div class="footer">
-    <ul class="footer-button-plus">
-      <input type="file" id="file" class="inputfile" />
-      <label for="file" class="input-plus">+</label>
-    </ul>
- </div>
 </template>
 
-
 <script>
-import './Post.js';
+import Container from './components/Container.vue';
+import posts from './Post.js'; 
+import axios from 'axios';
 
 export default {
-  name : 'App',
-  components : {
+  name: 'App',
+  components: {
+    Container
+  },
+  data() {
+    return {
+      posts
+    };
+  },
+  methods:{
+    more() {
+      axios.get('https://qkrwpgus.github.io/vue/more0.json')
+      .then((result)=>{
+        this.posts.push(...result.data);
+      })  
+    }
   }
-}
+};
 </script>
