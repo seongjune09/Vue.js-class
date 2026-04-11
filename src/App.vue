529a8a28eac050b78445c2d1879c15bb9ac98e93
박성준 <freestarjun@gmail.com>
2026-04-11 23:19:27 +0900

    2026/4/11/App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,49 @@
-// App.vue
 <template>
-  <nav class="navbar navbar-light bg-light px-3">
-    <span class="navbar-brand fw-bold">Vuelog</span>
-  </nav>
+  <div>
+    <div class="header">
+      <ul class="header-button-left">
+        <li>Cancel</li>
+      </ul>
+      <ul class="header-button-right">
+        <li>Next</li>
+      </ul>
+      <img src="./assets/logo.svg" class="logo" />
+    </div>
 
-  <router-view :blogs="블로그글"></router-view> 
+    <Container :posts="posts" />
+    <button @click="more">더보기</button>
 
+    <div class="footer">
+      <ul class="footer-button-plus">
+        <input type="file" id="file" class="inputfile" />
+        <label for="file" class="input-plus">+</label>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-import data from './assets/blog';
+import Container from './components/Container.vue';
+import posts from './Post.js'; 
+import axios from 'axios';
 
 export default {
   name: 'App',
+  components: {
+    Container
+  },
   data() {
     return {
-      블로그글: data,
+      posts
+    };
+  },
+  methods:{
+    more() {
+      axios.get('https://qkrwpgus.github.io/vue/more0.json')
+      .then((result)=>{
+        this.posts.push(...result.data);
+      })  
     }
   }
-}
+};
 </script>
