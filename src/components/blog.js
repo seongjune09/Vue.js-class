6f5db641d22ba39eaf9ada87de218bec4d9ffbfe
박성준 <freestarjun@gmail.com>
2026-04-19 23:08:56 +0900

    2026/4/17

---
+++ src/components/blog.js
@@ -0,0 +1,41 @@
+//Container.vue
+<template>
+    <div class="container">
+        <div v-if="step==0">
+            <Post v-for="(post, index) in posts" :key="index" :post="post" />
+        </div>
+
+        <!-- 필터선택페이지 -->
+         <div v-if="step==1">
+        <div class="upload-image"></div>
+        <div class="filters">
+            <div class="filter-1"></div>
+            <div class="filter-1"></div>
+            <div class="filter-1"></div>
+            <div class="filter-1"></div>
+            <div class="filter-1"></div>
+        </div>
+        </div>
+
+        <!-- 글작성페이지 -->
+        <div v-if="step==2">
+        <div class="upload-image"></div>
+        <div class="write">
+            <textarea class="write-box">write!</textarea>
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Post from './Post.vue';
+
+export default {
+    name: 'Container',
+    components: { Post },
+    props: {
+        step: Number,
+        posts: Array,
+    }
+};
+</script>
