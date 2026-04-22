0b6db109c65ea07acf468acecde4b9c3d4142fde
박성준 <freestarjun@gmail.com>
2026-04-22 20:52:28 +0900

    2026/4/22

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,23 +1,41 @@
 <template>
-  <div>
-    <Post
-      v-for="(post, index) in posts"
-      :key="index"
-      :post="post"
-    />
-  </div>
+    <div class="container">
+        <div v-if="step==0">
+            <Post v-for="(post, index) in posts" :key="index" :post="post" />
+        </div>
+
+        <!-- 필터선택페이지 -->
+         <div v-if="step==1">
+        <div class="upload-image" :style="{ backgroundImage: `url(${image})` }"></div>
+        <div class="filters">
+            <div class="filter-1"></div>
+            <div class="filter-1"></div>
+            <div class="filter-1"></div>
+            <div class="filter-1"></div>
+            <div class="filter-1"></div>
+        </div>
+        </div>
+
+        <!-- 글작성페이지 -->
+        <div v-if="step==2">
+        <div class="upload-image" :style="{ backgroundImage: `url(${image})` }"></div>
+        <div class="write">
+            <textarea class="write-box">write!</textarea>
+        </div>
+        </div>
+    </div>
 </template>
 
 <script>
 import Post from './Post.vue';
 
 export default {
-  name: 'Container',
-  components: {
-    Post
-  },
-  props: {
-    posts: Array
-  }
+    name: 'Container',
+    components: { Post },
+    props: {
+        step: Number,
+        posts: Array,
+        image: String,
+    }
 };
 </script>
